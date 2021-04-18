3a433b769ac9350ab274e59f0c31bca3182a09d8
Richard Liebscher <r1tschy@posteo.de>
2021-04-18 18:48:33 +0200

    Improve footer

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -1,16 +1,23 @@
+<style lang="scss">
+  .footer {
+      border-top: 1px solid hsl(0, 0%, 86%);
+      background-color: hsl(0, 0%, 99%);
+  }
+</style>
+
 <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <strong>Game of Estimates</strong> by <a href="https://github.com/R1tschY">Richard Liebscher</a>. The source code is licensed under
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        <strong>Game Of Estimates</strong> by <a href="https://github.com/R1tschY">Richard Liebscher</a>.
       </p>
       <p>
+        The source code is licensed under <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
         <span class="icon-text">
-            <span class="icon">
-              <i class="fab fa-github"></i>
-            </span>
-            <a href="https://github.com/R1tschY/game_of_estimates">Github</a>
+          <span class="icon">
+            <i class="fab fa-github"></i>
           </span>
+          <a href="https://github.com/R1tschY/game_of_estimates">Github</a>
+        </span>
       </p>
     </div>
   </footer>
