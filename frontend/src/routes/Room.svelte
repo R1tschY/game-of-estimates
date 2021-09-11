259fca9897f66e8dd84f6791415d7612a7ff3558
Richard Liebscher <r1tschy@posteo.de>
2021-09-11 15:05:43 +0200

    Fix layout of estimation cards

--- frontend/src/routes/Room.svelte
+++ frontend/src/routes/Room.svelte
@@ -1,100 +1,3 @@
-<style lang="scss">
-    $card-width: 63mm * 0.25;
-    $card-height: 88mm * 0.25;
-
-    $tt-gray: #63666a;
-    $tt-orange: #ed8b00;
-    $tt-petrol: #208CA3;
-
-    .card-inner {
-        border: 1px solid #eee;
-        border-radius: 7px 0 7px 0;
-        margin: 3px;
-        line-height: calc(#{$card-height} - 10px);
-        vertical-align: middle;
-    }
-
-    .game-card {
-        width: $card-width;
-        height: $card-height;
-        padding: 0;
-        font-size: 1.7em;
-        text-align: center;
-        display: inline-block;
-        // line-height: 3em;
-        margin: 0rem 0.25rem 0.5rem 0.25rem;
-        border: 1px solid $tt-gray;
-        box-shadow: 0 0em 0.5em -0.125em rgba(10, 10, 10, 0.1);
-        outline: 0;
-        transition: transform 1s, margin-top 0.2s;
-        border-radius: 10px 0 10px 0;
-        transform: rotateY(0deg);
-    }
-
-    .game-card.selectable {
-        cursor: pointer;
-    }  
-
-    .game-card.selected {
-        background-color: #{adjust-color($tt-orange, $lightness: 40%)};
-        //color: white;
-        margin-top: -0.5rem;
-    }
-
-    .game-card-item {
-        display: inline-block;
-        vertical-align: top;
-        white-space: nowrap;
-        list-style: none;
-    }
-
-    .game-card-back {
-        background-color: $tt-orange;
-        color: black;
-        padding: 2px;
-        backface-visibility: hidden;
-        transform: rotateY(180deg);
-        position: absolute;
-    }
-
-    .game-card-front {
-        background-color: white;
-        padding: 2px;
-        backface-visibility: hidden;
-        transform: rotateY(0deg);
-        position: absolute;
-    }
-
-    .game-card-normal {
-        position: unset;
-        background-color: white;
-    }
-
-    .backcover > .game-card-back {
-        transform: rotateY(0deg);
-    }
-
-    .backcover > .game-card-front {
-        transform: rotateY(180deg);
-    }
-
-    .hidden > .game-card {
-        display: none;
-    }
-
-    .empty {
-        border: 2px solid transparent;
-        box-shadow: inset 0 0em 0.5em -0.125em rgba(10, 10, 10, 0.1);
-        z-index: -10;
-        position: relative;
-    }
-
-    .card-row {
-        height: calc(#{$card-height} + 4rem);
-        margin: 10px 0;
-    }
-</style>
-
 <script lang="ts">
     import { playerId, vote, voter, debug, players, gameState, name as nameStore } from '../stores'
     import Header from '../components/Header.svelte'
@@ -199,10 +102,10 @@
                     <div>
                         <div class:backcover={!open} class:hidden={!player.vote}>
                             <div class="game-card game-card-back">
-                                <div class="card-inner">♠️</div>
+                                <div class="game-card-inner"><img class="game-card-symbol" src="/clubs.svg" alt="" /></div>
                             </div>
                             <div class="game-card game-card-front">
-                                <div class="card-inner">{player.vote ? player.vote : '\xA0'}</div>
+                                <div class="game-card-inner">{player.vote ? player.vote : '\xA0'}</div>
                             </div>
                         </div>
                         <div class="game-card empty"></div>
@@ -229,7 +132,7 @@
                                 on:click={() => setVote(card)}
                                 class:selected={$vote === card}
                                 class:selectable={!open}>
-                                <div class="card-inner">{card}</div>
+                                <div class="game-card-inner">{card}</div>
                             </button>
                         </li>
                     {/each}
