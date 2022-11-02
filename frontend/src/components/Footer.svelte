2a7d6330ddf43a94400a734707c633eeb1f0c49e
Richard Liebscher <r1tschy@posteo.de>
2022-11-02 21:32:41 +0100

    Fix GitHub icon in footer

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import GitHub from '../icons/GitHub.svelte'
+</script>
+
 <style lang="scss">
   .footer {
       border-top: 1px solid hsl(0, 0%, 86%);
@@ -6,18 +10,14 @@
 </style>
 
 <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>Game Of Estimates</strong> by <a href="https://github.com/R1tschY">Richard Liebscher</a>.
-      </p>
-      <p>
-        The source code is licensed under <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-        <span class="icon-text">
-          <span class="icon">
-            <i class="fab fa-github"></i>
-          </span>
-          <a href="https://github.com/R1tschY/game_of_estimates">Github</a>
-        </span>
-      </p>
-    </div>
-  </footer>
+  <div class="content has-text-centered">
+    <p>
+      <strong>Game Of Estimates</strong> by <a href="https://github.com/R1tschY">Richard Liebscher</a>.
+    </p>
+    <p>
+      The source code is licensed under <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+      <div>
+        <a href="https://github.com/R1tschY/game_of_estimates"><span><GitHub class="footer-icon"/></span></a>
+      </div>
+  </div>
+</footer>
