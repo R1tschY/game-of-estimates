18db244b91700cf410c1683eafd4cc0da4d0e033
Richard Liebscher <r1tschy@posteo.de>
2022-12-26 12:41:07 +0100

    Improve String.format

--- frontend/src/ext/string.ts
+++ frontend/src/ext/string.ts
@@ -2,8 +2,7 @@ export {}
 
 const PLACEHOLDER_RE = /{([0-9]+)}/g
 
-String.prototype.format = function () {
-    const args = arguments
+String.prototype.format = function (...args) {
     return this.replace(PLACEHOLDER_RE, function (match, index) {
         const arg = args[index]
         return arg === undefined ? '' : arg
