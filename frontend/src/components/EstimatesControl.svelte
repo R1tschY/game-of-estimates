3a93c0c20e79d92a2d6b1fe5d6dcba2857a9dae6
Richard Liebscher <r1tschy@posteo.de>
2022-12-26 12:04:12 +0100

    Improve selection on mobile devices

--- frontend/src/components/EstimatesControl.svelte
+++ frontend/src/components/EstimatesControl.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-    import { vote, gameState } from '../stores'
-    import { get_deck as getDeck } from '../deck'
+    import {vote, gameState} from '../stores'
+    import {get_deck as getDeck} from '../deck'
 
-    $: open = $gameState && $gameState.open
+    $: open = $gameState ? $gameState.open : false
     $: cards = $gameState ? getDeck($gameState.deck).cards : []
+
+    let touch_hover: string | null = null;
+    let handNode
 
     function setVote(value: string | null) {
         if (!open) {
@@ -12,18 +15,44 @@
             })
         }
     }
+
+    function touchHover(evt: TouchEvent) {
+        if (evt.touches.length === 1) {
+            const touch = evt.touches.item(0)
+            const cardsValue = cards;
+            const rect = handNode.getBoundingClientRect()
+
+            let fpos = (touch.clientX - rect.left) * cardsValue.length / (rect.right - rect.left);
+            const pos = Math.min(cardsValue.length - 1, Math.max(0, Math.round(fpos)))
+            touch_hover = cardsValue[pos]
+            evt.preventDefault()
+        }
+    }
+
+    function touchHoverEnd() {
+        setVote(touch_hover)
+        touch_hover = null
+    }
+
+    function touchHoverCancel() {
+        setVote(null)
+        touch_hover = null
+    }
 </script>
 
-<ul class="hand">
+<ul class="hand" bind:this={handNode} on:touchstart={touchHover} on:touchmove={touchHover} on:touchend={touchHoverEnd}
+    on:touchcancel={touchHoverCancel}>
     {#each cards as card}
         <li class="hand-entry">
             <div class="game-card-item hand-entry-inner">
                 <button
-                    class="game-card game-card-normal"
-                    type="button"
-                    on:click={() => setVote(card)}
-                    class:selected={$vote === card}
-                    class:selectable={!open}
+                        class="game-card game-card-normal"
+                        type="button"
+                        on:click={() => setVote(card)}
+                        on:touchmove={touchHover}
+                        class:selected={$vote === card}
+                        class:touch-hover={touch_hover === card}
+                        class:selectable={!open}
                 >
                     <span class="game-card-inner">{card}</span>
                 </button>
