125214b932f35a454a066498d04ae6974bb6554e
Richard Liebscher <r1tschy@posteo.de>
2026-03-16 23:48:55 +0100

    Fix debug build

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -18,11 +18,11 @@ use prometheus_client::registry::Registry;
 use rust_embed::Embed;
 use serde::Deserialize;
 use std::sync::Arc;
-#[cfg(not(debug_assertions))]
+#[cfg(debug_assertions)]
 use tower::layer::util::Stack;
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
-#[cfg(not(debug_assertions))]
+#[cfg(debug_assertions)]
 use tower_http::cors::CorsLayer;
 use tower_serve_assets::embed::EmbedCatalog;
 use tower_serve_assets::ServeAssets;
